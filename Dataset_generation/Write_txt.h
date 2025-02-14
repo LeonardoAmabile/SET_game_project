a62159b776148c8136a4fdf938c32f66ca900fdf
Jacopo Omodei <jacopo.omodei@gmail.com>
2025-02-14 09:36:02 +0100

    Implemented a counter for null-set tables in Write_txt.h

--- Dataset_generation/Write_txt.h
+++ Dataset_generation/Write_txt.h
@@ -24,10 +24,17 @@ void write_data(const string& filename, const vector<int>& SETs_count, const vec
     file << "# Number of SETs in each Table" << "\n\n";
 
     // Write the number of SETs for each matrix in the first row from SETs_count
+    vector<int> Zeros_count;
     for (const auto& count : SETs_count) {
         file << count << " ";  // Each count corresponds to a table
+        if (count == 0) {
+            Zeros_count.push_back(count);
+        }
     }
     file << "\n\n";  // New line after the number of SETs
+    file << "Number of Tables with 0 SETs: " << Zeros_count.size() << "/" << SETs_count.size() << "\n\n";
+    file << "Probability of no SETs: " << (double)100*Zeros_count.size() / SETs_count.size() << "% \n\n";
+    file << "\n\n";  // New line after the number of tables with no SETs
 
     // Write description of the Tables:
     file << "# Tables considered" << "\n\n";
